b1fd5309f25cd8788b7992e35ecff95db6d22705
Lucas Ribeiro <lmr2199@gmail.com>
2020-06-04 15:20:45 -0300

    Tabelas criadas

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,7 +1,16 @@
-export async function up(){
+import Knex from 'knex';
+export async function up(knex: Knex){
     // CRIAR TABELA
+    return knex.schema.createTable('items', table =>{
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+
+    });
+
 
 }
 
-export async function down(){
+export async function down(knex: Knex){
+    return knex.schema.dropTable('items')
 }
