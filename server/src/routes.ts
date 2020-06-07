28c2b54e50bd946d059664b5e0c29b97b3102d99
Lucas Ribeiro <lmr2199@gmail.com>
2020-06-06 21:32:59 -0300

    Sem Controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,58 @@
-import express from 'express';
-
+import express, { request } from 'express';
+import knex from './database/connection';
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-    console.log('Ouvindo endpoint');
-    response.json(['Charlie','Alan','Gisele']);
-    
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    });
+    return response.json(serializedItems)
 });
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    })
+    const point_id = insertIds[0];
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    });
+
+    await trx('point_items').insert(pointItems);
+    return response.json({ success: true })
+});
+
+
 
 
 export default routes;
