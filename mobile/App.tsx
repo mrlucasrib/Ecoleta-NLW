f74c94d810bebc060b8807d9235bc9e44be56217
Lucas Ribeiro <lmr2199@gmail.com>
2020-06-09 00:05:48 -0300

    Pagina Home criada

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import Home from './src/pages/Home'
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  return (
+    <>
+      <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
+      <Home />
+    </>
+  );
+}
